098b2f049fb4adb919c66546f5543e8dea695150
wei.yang <ctykwz@outlook.com>
2017-11-07 19:33:38 +0800

    修改消费者配置

---
+++ src/main/java/com/ctyk/mobile/template/module/rabbitmq/Consumer.java
@@ -0,0 +1,51 @@
+package com.ctyk.mobile.template.module.rabbitmq;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.*;
+
+/**
+ *
+ * @author wei.yang on 2017/11/6
+ */
+@Component
+public class Consumer implements Closeable {
+
+    private static final Logger logger = Logger.getLogger(Consumer.class);
+
+    private ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("thread-%d").build();
+
+    private ExecutorService executors = new ThreadPoolExecutor(10,
+            20,
+            0L,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingDeque<>(1024),
+            threadFactory);
+
+    public void onMessage(Object object) {
+        executors.submit(new HandlerWithJson(object));
+    }
+
+    @Override
+    public void close() throws IOException {
+        executors.shutdown();
+    }
+
+    private class HandlerWithJson implements Runnable {
+
+        private Object message;
+
+        HandlerWithJson(Object message) {
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            logger.info("msg:\t" + message);
+        }
+    }
+}
